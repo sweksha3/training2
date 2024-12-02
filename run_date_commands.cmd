a845f3c34bfa3880cc6a5e87375687e55e0514d3
Sweksha Maurya <149899840+sweksha3@users.noreply.github.com>
2024-12-02 16:42:56 +0530

    Create run_date_commands.cmd

---
+++ run_date_commands.cmd
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Go to the main directory (root directory)
+cd /
+
+# Create the folder in the main directory
+ipconfig
+mkdir "popcorns"
+
+echo "Folder 'Folder with Azure pipeline' created successfully in the main directory!"
